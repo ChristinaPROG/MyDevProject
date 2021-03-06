96b8a155677cf7a047e3456d1064a133c8689ec1
ChristinaPROG <cwag.ramirez4@gmail.com>
2021-03-06 12:35:59 -0500

    commit4

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 3 ; i++) {
-            System.out.println ("Hello batch 8" );
+            ystem.out.println ("Hello batch 8" );
             Thread.sleep ( 2000 );
 
 
